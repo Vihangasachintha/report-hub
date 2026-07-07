911b340c422d126dae4724efc04b0519a80b6002
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-07-07 08:10:16 +0530

    Add RoleMiddleware to handle user role authorization

---
+++ backend/app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RoleMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  Closure(Request): (Response)  $next
+     */
+    public function handle($request, Closure $next, ...$roles)
+    {
+        if (!in_array(auth('api')->user()->role, $roles)) {
+            return response()->json(['message' => 'Forbidden'], 403);
+        }
+        return $next($request);
+    }
+}
