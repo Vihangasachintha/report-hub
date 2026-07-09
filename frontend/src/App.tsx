05767df709ac56c3cff885dd3a8d8f0f5501dba5
Vihanga Sachintha <vihanga1sachintha@gmail.com>
2026-07-09 11:34:17 +0530

    feat: restructure App component to implement routing with login navigation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
- 
   return (
-    <div>
-      <h1>Weekly Report Generator</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* When app starts, go to login */}
+        <Route path="/" element={<Navigate to="/login" replace />} />
+
+        {/* Login page */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Dashboard page */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
